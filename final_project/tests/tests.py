f8b71960819971ff7179fb590fa63db7584ba6c8
BengGit <benglei03606@gmail.com>
2023-04-29 09:35:15 -0400

    Final changes

---
+++ final_project/tests/tests.py
@@ -0,0 +1,29 @@
+
+import sys
+import os
+import unittest
+
+# Get the absolute path to the directory containing this file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add the directory containing translator.py to the Python path
+translator_dir = os.path.join(current_dir, '../machinetranslation')
+sys.path.append(translator_dir)
+
+from translator import english_to_french, french_to_english
+
+class TestTranslation(unittest.TestCase):
+    def test_english_to_french_null_input(self):
+        self.assertEqual(english_to_french(None), None)
+    
+    def test_english_to_french_translation(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    
+    def test_french_to_english_null_input(self):
+        self.assertEqual(french_to_english(None), None)
+    
+    def test_french_to_english_translation(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        
+if __name__ == '__main__':
+    unittest.main()
